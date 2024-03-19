d8a4abc2339ca516ba9a2d01fe6bc8cf61987b18
techeca <jp.vasquez@outlook.com>
2024-03-19 16:57:41 -0300

    ADD: Font Manrope.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}','node_modules/flowbite-react/lib/esm/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        regular: ['Regular'],
+        bold: ['Bold'],
+        extrabold: ['ExtraBold'],
+        extralight: ['ExtraLight'],
+        light: ['Light'],
+        medium: ['Medium'],
+        regular: ['Regular'],
+        semibold: ['SemiBold']
+      },
+      backgroundImage: {
+        'loginImage': "url('/img/loginfondo.png)"
+      }
+    },
   },
   plugins: [require('flowbite/plugin')],
 }
