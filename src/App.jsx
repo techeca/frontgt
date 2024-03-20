0039e386742fb9d4483540fa54ec774459413c60
techeca <jp.vasquez@outlook.com>
2024-03-20 01:00:42 -0300

    UPDATE: Se eliminan imports y elementos innecesarios.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-//import './App.css'
-import { Button, Label, TextInput, Checkbox } from 'flowbite-react'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      {<Login />}
-      {/*<Dashboard />*/}
+      {/*<Login />*/}
+      {<Dashboard />}
     </>
   )
 }
