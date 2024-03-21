12bb04a23ec9fb095365da3302eb77955ca848ad
techeca <jp.vasquez@outlook.com>
2024-03-20 23:46:20 -0300

    UPDATE: Limpieza y orden de componentes.

--- src/components/UserLogin/index.jsx
+++ src/components/UserLogin/index.jsx
@@ -1,7 +1,7 @@
-import { Button, Label, TextInput, Checkbox, Card } from 'flowbite-react'
-import { HiMail, HiLockClosed } from 'react-icons/hi'
+import { Button, Card } from 'flowbite-react';
 import { FcGoogle } from "react-icons/fc";
 import { IoLogoGithub } from "react-icons/io5";
+import LoginForm from '../forms/LoginForm';
 
 export default function UserLogin(){
 
@@ -11,14 +11,14 @@ export default function UserLogin(){
                 <p className='font-semibold text-slate-700'>GatoCósmico</p>
             </div>
             <h1 className='text-center text-2xl font-bold mb-2 text-cyan-950'>Bienvenido</h1>
-            {/*<h2 className=''>¿No tienes cuenta? <span className='text-cyan-700'>Regístrate</span></h2>*/}
+            {<h2 className=''>¿No tienes cuenta? <a href='/register' className='text-cyan-700 hover:underline'>Regístrate</a></h2>}
             <div>
                 <div className='flex flex-col gap-6'>
-                    <Button color={'light'} type='submit'>
+                    <Button disabled color={'light'} type='submit' className='cursor-not-allowed'>
                         <FcGoogle className='mr-2 h-5 w-5' />
                         Ingresar con Google
                     </Button>
-                    <Button color={'light'} type='submit'>
+                    <Button disabled color={'light'} type='submit' className='cursor-not-allowed'>
                         <IoLogoGithub className='mr-2 h-5 w-5' />
                         Ingresar con Github
                     </Button>
@@ -29,30 +29,7 @@ export default function UserLogin(){
                 <a>or</a>
                 <hr/>
             </div>
-            <form className="flex flex-col gap-6">
-                <div className='flex md:flex-row flex-col md:gap-6 gap-3'>
-                    <div>
-                        <div className="mb-2 block">
-                        <Label htmlFor="txtCorreo" value="Correo" className='text-sm' />
-                        </div>
-                        <TextInput className='fill-cyan-700' id="txtCorreo" icon={HiMail} type="email" placeholder="nombre@correo.com" required />
-                    </div>
-                    <div>
-                        <div className="mb-2 block">
-                        <Label htmlFor="txtContrasena" value="Contraseña" className='text-sm' />
-                        </div>
-                        <TextInput id="txtContrasena" type="password" icon={HiLockClosed} placeholder='********' required />
-                    </div>
-                 </div>
-                <div className="flex items-center justify-between">
-                    <div className='flex items-center gap-2'>
-                        <Checkbox id="chbRecordar" className='hover:cursor-pointer' />
-                        <Label htmlFor="chbRecordar">Recordar</Label>
-                    </div>
-                     <a className='text-sm font-light hover:cursor-pointer hover:underline text-cyan-700'>Recuperar contraseña</a>
-                </div>
-                <Button type="submit">Enviar</Button>
-            </form>
+            <LoginForm />
         </Card>           
     )
 }
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,4 @@
 import UserLogin from "../../components/UserLogin";
-import { HiOutlineQuestionMarkCircle } from "react-icons/hi"
-import { Button, Label, TextInput, Checkbox, Card } from 'flowbite-react'
-import { HiMail, HiLockClosed } from 'react-icons/hi'
-import { FcGoogle } from "react-icons/fc";
-import { IoLogoGithub } from "react-icons/io5";
 
 export default function Login(){
 
@@ -15,11 +10,6 @@ export default function Login(){
                     <img width={'auto'} height='auto' src='/img/workers.png'></img>
                 </div>
             </div>
-            {/*<div className={`w-full bg-[url('/img/loginfondo.png')] bg-cover`}></div>
-                <UserLogin />
-                <div className='absolute bottom-0 right-0 mr-12 mb-12 w-12 h-12 bg-white/50 rounded-full'>
-                <HiOutlineQuestionMarkCircle className='text-5xl opacity-90 text-cyan-900/80 hover:text-cyan-900/50 cursor-pointer' />
-            </div>*/}
         </div>
     )
 }
